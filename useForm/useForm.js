dfe1c02448dd5d9c28302259dfcaa5cfa9e0bcfc
Sergio <57294355+MAGOLEINAD@users.noreply.github.com>
2024-01-11 18:06:05 -0300

    Update useForm.js
    Agregada version TS

--- useForm/useForm.js
+++ useForm/useForm.js
@@ -22,3 +22,27 @@ export const useForm = ( initialState = {} ) => {
     return [ values, handleInputChange, reset ];
 
 }
+
+
+//TYPESCRIPT
+
+import { useState } from 'react';
+
+export const useForm = <T extends Object>( initState: T ) => {
+    
+    const [state, setState] = useState( initState );
+
+    const onChange = <K extends Object>( value: K, field: keyof T ) => {
+        setState({
+            ...state,
+            [field]: value
+        });
+    }
+
+    return {
+        ...state,
+        form: state,
+        onChange,
+    }
+
+}
